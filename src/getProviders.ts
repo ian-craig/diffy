807c5dff7ca41e0a0313f65e467a6f9c1a7e75e3
Ian Craig <ian-craig@outlook.com>
2019-08-31 18:26:09 -0700

    Find external providers through environment variable

--- src/getProviders.ts
+++ src/getProviders.ts
@@ -1,28 +1,29 @@
 import * as path from 'path';
 import * as fs from 'fs';
 import { ProviderFactory } from "./DataStructures/IProvider";
-import { app } from "electron";
 
 export const getProviders = () => {
     // Built in providers
     const providers: ProviderFactory[] = [
         require('./Providers/GitProvider').default,
     ];
-    
-    const providersRoot = app.isPackaged ? `${path.dirname(app.getPath('exe'))}-providers` : `${app.getAppPath()}-providers`;
-    console.log(`Searching for providers in ${providersRoot}`);
 
-    if (fs.existsSync(providersRoot)) {
-        fs.readdirSync(providersRoot).forEach(providerDir => {
-            if (fs.existsSync(path.join(providersRoot, providerDir, "package.json"))) {
-                const pkg = require(path.join(providersRoot, providerDir, "package.json"));
-                const mainFileFullPath = path.join(providersRoot, providerDir, pkg.main);
-                if (pkg.main && fs.existsSync(mainFileFullPath)) {
-                    console.log(`Adding provider ${mainFileFullPath}`);
-                    providers.push(require(mainFileFullPath).default);
-                }
+    const providerPaths = (process.env.MONACO_DIFF_PROVIDERS || "")
+        .split(";")
+        .map(p => p.trim())
+        .filter(p => !!p);
+
+    for (const providerDir of providerPaths) {
+        console.debug(`Searching for provider in ${providerDir}`);
+        const packageJsonPath = path.join(providerDir, "package.json");
+        if (fs.existsSync(packageJsonPath)) {
+            const pkg = require(packageJsonPath);
+            const mainFileFullPath = path.join(providerDir, pkg.main);
+            if (pkg.main && fs.existsSync(mainFileFullPath)) {
+                console.debug(`Adding provider ${mainFileFullPath}`);
+                providers.push(require(mainFileFullPath).default);
             }
-        });
+        }
     }
 
     return providers;
