7dd1afc6dd79949492865cf2e2169492c3bf7abd
Ian Craig <ian-craig@outlook.com>
2019-11-03 17:07:21 -0800

    Update basic UI Fabric selection to work with Redux store

--- src/Components/FileList.tsx
+++ src/Components/FileList.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 import { FileListItem } from "./FileListItem";
 import { GroupedList, IGroup } from "office-ui-fabric-react/lib/GroupedList";
-import { IColumn, DetailsRow } from "office-ui-fabric-react/lib/DetailsList";
+import { IColumn, DetailsRow, ISelection } from "office-ui-fabric-react/lib/DetailsList";
 import { Selection, SelectionMode, SelectionZone } from "office-ui-fabric-react/lib/Selection";
 import { connect } from "react-redux";
 import { AppState } from "../state/Store";
 import { changeListsSelector, IChangeListModel } from "../state/ChangeLists";
 import { setSelectedDiff } from "../state/Selected";
 import { Dispatch, AnyAction } from "redux";
-import "./FileList.css";
+import { DiffModel } from "../Utils/DiffModel";
 
-const List = require("react-list-select").default;
+import "./FileList.css";
 
 interface IProps {
   changeLists: IChangeListModel[];
@@ -19,46 +19,41 @@ interface IProps {
 }
 
 interface IState {
-  selected: number[];
   items: Item[];
-  disabled: number[];
   groups: IGroup[];
+  selection: ISelection;
 }
 
 interface Item {
   key: string;
-  changelist: IChangeList;
-  diffSpec: IDiffSpec;
-  diffModel?: DiffModel;
+  changelist: IChangeListModel;
+  diffModel: DiffModel;
 }
 
 class FileListComponent extends React.Component<IProps, IState> {
-  private itemMap = new Map<number, string>();
-  private _selection = new Selection();
-
   public constructor(props: IProps) {
     super(props);
 
     this.state = {
-      selected: [],
       items: [],
-      disabled: [],
       groups: [],
+      selection: new Selection({ onSelectionChanged: this.onSelectionChanged }),
     };
   }
 
   public componentDidMount() {
     this.updateList();
+
+    if (this.props.selectedDiffId !== undefined) {
+      this.state.selection.selectToKey(this.props.selectedDiffId);
+    }
   }
 
   private updateList() {
-    this.itemMap.clear();
     const items: Item[] = [];
-    const disabled: number[] = [];
     const groups: IGroup[] = [];
 
-    this.props.changeLists.forEach((changelist, cIndex) => {
-      disabled.push(items.length);
+    this.props.changeLists.forEach(changelist => {
       groups.push({
         key: changelist.id,
         name: changelist.name,
@@ -66,42 +61,42 @@ class FileListComponent extends React.Component<IProps, IState> {
         count: changelist.files.length,
       });
 
-      cl.files.forEach(diffModel => {
-        const index = items.length;
-
-        this.itemMap.set(index, diffModel.id);
-        items.push(<FileListItem diffModel={diffModel} changelist={cl} />);
-      });
+      for (const diffModel of changelist.files) {
+        items.push({ key: diffModel.id, diffModel, changelist });
+      }
     });
 
     this.setState({
       items,
       groups,
-      disabled,
     });
-    this._selection.setItems(items);
 
-    const selectedIsInItems =
-      this.props.selectedDiffId !== undefined &&
-      Array.from(this.itemMap.values()).find(id => id === this.props.selectedDiffId) !== undefined;
-    if (!selectedIsInItems && this.itemMap.size > 0) {
-      this.onChange(Array.from(this.itemMap.keys())[0]);
-    }
+    this.state.selection.setItems(items, false);
   }
 
   public componentDidUpdate(oldProps: IProps) {
     if (this.props.changeLists !== oldProps.changeLists) {
       this.updateList();
     }
+    if (this.props.selectedDiffId !== undefined) {
+      const currentSelection = this.state.selection.getSelection();
+      if (currentSelection.length !== 1 || currentSelection[0].key !== this.props.selectedDiffId) {
+        this.state.selection.selectToKey(this.props.selectedDiffId);
+      }
+    } else if (this.state.items.length > 0) {
+      this.props.selectDiff(this.state.items[0].key);
+    }
   }
 
-  private readonly onChange = async (selectedIndex: number) => {
-    console.log("Selcting", selectedIndex);
-    this.setState({
-      selected: [selectedIndex],
-    });
-
-    this.props.selectDiff(this.itemMap.get(selectedIndex) as string);
+  private readonly onSelectionChanged = () => {
+    const selectedItems = this.state.selection.getSelection() as Item[];
+    console.log("Selcting", selectedItems);
+    if (selectedItems.length === 1) {
+      this.props.selectDiff(selectedItems[0].key);
+    } else if (selectedItems.length === 0 && this.state.items.length > 0 && this.props.selectedDiffId !== undefined) {
+      this.state.selection.setKeySelected(this.props.selectedDiffId, true, false);
+      this.forceUpdate();
+    }
   };
 
   private columns: IColumn[] = [
@@ -120,14 +115,13 @@ class FileListComponent extends React.Component<IProps, IState> {
     item: Item,
     itemIndex: number | undefined,
   ): JSX.Element => {
-    console.log("_onRenderCell", nestingDepth, item, itemIndex);
     return (
       <DetailsRow
         columns={this.columns}
         groupNestingDepth={nestingDepth}
         item={item}
         itemIndex={itemIndex || 0}
-        selection={this._selection}
+        selection={this.state.selection}
         selectionMode={SelectionMode.multiple}
         compact={true}
       />
@@ -135,14 +129,13 @@ class FileListComponent extends React.Component<IProps, IState> {
   };
 
   public render() {
-    console.log("Render", this.state.groups, this.state.items);
     return (
       <div id="file-list">
-        <SelectionZone selection={this._selection} selectionMode={SelectionMode.multiple}>
+        <SelectionZone selection={this.state.selection} selectionMode={SelectionMode.multiple}>
           <GroupedList
             items={this.state.items}
             onRenderCell={this._onRenderCell}
-            selection={this._selection}
+            selection={this.state.selection}
             selectionMode={SelectionMode.multiple}
             groups={this.state.groups}
             compact={true}
